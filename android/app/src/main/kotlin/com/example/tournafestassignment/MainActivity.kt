f2c0ab5be5892d2cdb76f2d13599953749b730e1
Spyy004 <54628130+Spyy004@users.noreply.github.com>
2021-09-13 17:57:28 +0530

    AppBar and Bottom NavBar Created

---
+++ android/app/src/main/kotlin/com/example/tournafestassignment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tournafestassignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+TextStyle bodyText = TextStyle(color: Colors.black54,fontWeight: FontWeight.bold,fontSize: 16);
